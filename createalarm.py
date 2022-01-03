3c700b03f42e4160d205246c71547c43e50322a5
Bruno Nuteer Cupido <nuteer@hotmail.com>
2022-01-02 22:59:48 -0300

    create alarm using boto3

---
+++ createalarm.py
@@ -0,0 +1,25 @@
+import boto3
+
+# create cloudwatch client
+cloudwatch = boto3.client('cloudwatch')
+
+# Create alarm for CPU
+cloudwatch.put_metric_alarm(
+	AlarmName='web_server_CPU_Utilization',
+	ComparisonOperator='GreaterThanThreshold',
+	EvaluationPeriods=1,
+    MetricName='CPUUtilization',
+    Namespace='AWS/EC2',
+    Period=60,
+    Statistic='Average',
+    Threshold=70.0,
+    ActionsEnabled=False,
+    AlarmDescription='Alarm when server CPU exceeds 70%',
+    Dimensions=[
+        {
+            'Name': 'InstanceId',   # Name of the dimension
+            'Value': 'INSTANCE_ID'  # replace with your instance id
+        },
+    ],
+    Unit='Percent',
+)
